45f2658508616b9cd25ad45df1bed058c749ce7b
Zac Fermanis <Zac@Zac-Desktop>
2016-12-05 19:54:31 -0500

    Update 2

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -14,7 +14,6 @@
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
-        vm.slides = [];
         vm.currIndex = 0;
 
         $scope.myInterval = 5000;
@@ -37,55 +36,9 @@
             $state.go('register');
         }
 
-        $scope.addSlide = function() {
-            var newWidth = 600 + vm.slides.length + 1;
-            vm.slides.push({
-                image: '//unsplash.it/' + newWidth + '/300',
-                text: ['Nice image','Awesome photograph','That is so cool','I love that'][vm.slides.length % 4],
-                id: vm.currIndex++
-            });
-        };
 
-        $scope.randomize = function() {
-            var indexes = generateIndexesArray();
-            assignNewIndexesToSlides(indexes);
-        };
 
-        for (var i = 0; i < 4; i++) {
-            $scope.addSlide();
-        }
 
-        // Randomize logic below
-
-        function assignNewIndexesToSlides(indexes) {
-            for (var i = 0, l = vm.slides.length; i < l; i++) {
-                vm.slides[i].id = indexes.pop();
-            }
-        }
-
-        function generateIndexesArray() {
-            var indexes = [];
-            for (var i = 0; i < currIndex; ++i) {
-                indexes[i] = i;
-            }
-            return shuffle(indexes);
-        }
-
-        // http://stackoverflow.com/questions/962802#962890
-        function shuffle(array) {
-            var tmp, current, top = array.length;
-
-            if (top) {
-                while (--top) {
-                    current = Math.floor(Math.random() * (top + 1));
-                    tmp = array[current];
-                    array[current] = array[top];
-                    array[top] = tmp;
-                }
-            }
-
-            return array;
-        }
 
 
     }
